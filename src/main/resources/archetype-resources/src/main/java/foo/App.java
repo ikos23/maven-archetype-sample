ba54839e8d543ab34a4b6bf2a21697a65c2d6847
Ivan Kos <ikos2201@gmail.com>
2018-02-05 22:14:15 +0200

    FOURTH Add source files to the project structure.

---
+++ src/main/resources/archetype-resources/src/main/java/foo/App.java
@@ -0,0 +1,12 @@
+package ${groupId}.foo;
+
+/**
+ * This is just a simple src java file.
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/resources/archetype-resources/src/test/java/test/foo/AppTest.java
@@ -0,0 +1,8 @@
+package ${groupId}.test.foo;
+
+/**
+ * This is just a simple src java file.
+ */
+public class AppTest {
+
+}
